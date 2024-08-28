998251eef863a109e9af45cda2f4d858bd26d333
10054645 <kavitesh.kamboj@thalesgroup.com>
2024-08-28 13:52:09 +0530

    add ascii view to text_to_bytes.py

--- cryptography/helper_functions/text_to_bytes.py
+++ cryptography/helper_functions/text_to_bytes.py
@@ -6,11 +6,11 @@ def bytes_to_str(some_bytes: bytes) -> str:
 
 def bytes_to_bits(some_bytes: bytes) -> str:
     """Converts a string to its binary representation in bits."""
-    return ' '.join(f'{byte:08b}' for byte in some_bytes)
+    return ' '.join(f'{byte:08b}' for byte in some_bytes) # 0-> padding, 8-> minimum width, b -> in binary format
 
 # Example usage:
 def main():
-    message = "Hello World!"
+    message = "Hello World! 09AZaz"
     message_bytes = str_to_bytes(message)    
     message_from_bytes = bytes_to_str(message_bytes)
     binary_view_of_bytes = bytes_to_bits(message_bytes)
@@ -18,6 +18,7 @@ def main():
     print(f"Message before conversion {message}")
     print(f"Bytes {message_bytes}")
     print(f"Binary {binary_view_of_bytes}")
+    print(f"Ascii {list(message_bytes)}")
     print(f"Message {message_from_bytes}")
 
 if __name__ == "__main__":
